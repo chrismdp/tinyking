44157d1d85d8173869d0fbc0c47468adb090501d
Chris Parsons <cp@gower.st>
2020-07-12 00:22:32 +0100

    Sensible webpack starting point
    Include linting with enforcement of coding style, babel transpiling to
    older browsers, scss + google font support, webpack-dev-server for
    development.
    
    Also allow the game to be made into a mobile app on an iPhone - just for
    kicks right now :)

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// These are needed as soon as we want to transpile down to default browsers
+//import "core-js/stable";
+//import "regenerator-runtime/runtime";
+
---
+++ webpack.common.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Tiny King",
+      meta: {
+        "viewport":"user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width",
+        "apple-mobile-web-app-capable": "yes"
+      }
+    })
+  ],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\/]node_modules[\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [ 'babel-loader', 'eslint-loader' ],
+      },
+      {
+        test: /\.scss$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ],
+      },
+      {
+        test: /\.(png|svg)$/,
+        use: [ 'file-loader' ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [ 'file-loader' ],
+      },
+    ],
+  },
+};

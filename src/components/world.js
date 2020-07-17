a6d7fa744e05296759de683bf3c9e2db62de00b9
Chris Parsons <cp@gower.st>
2020-07-18 00:59:12 +0100

    Fixed map position and events

--- src/components/world.js
+++ src/components/world.js
@@ -1,85 +1,85 @@
 import * as React from "react";
 import * as PIXI from "pixi.js";
+import PropTypes from "prop-types";
 
 import { Viewport } from "pixi-viewport";
 
 export function World(props) {
+  const [app, setApp] = React.useState(null);
   const container = React.useRef(null);
 
-  let app = new PIXI.Application({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    antialias: true,
-    resolution: window.devicePixelRatio || 1,
-    autoResize: true
-  });
-  console.log(app);
+  const createApp = (view) => {
+    console.log("Creating app");
+    let result = new PIXI.Application({
+      width: window.innerWidth,
+      height: window.innerHeight,
+      view: view,
+      antialias: true,
+      resolution: window.devicePixelRatio || 1,
+      autoResize: true
+    });
 
-  app.renderer.backgroundColor = 0x444444;
-  app.renderer.view.style.position = "absolute";
-  app.renderer.view.style.display = "block";
+    var viewport = new Viewport({
+      screenWidth: result.view.offsetWidth,
+      screenHeight: result.view.offsetHeight,
+      worldWidth: props.map.pointWidth(),
+      worldHeight: props.map.pointHeight(),
+      passiveWheel: false,
+      disableOnContextMenu: true
+    });
 
-  var viewport = new Viewport({
-    screenWidth: app.view.offsetWidth,
-    screenHeight: app.view.offsetHeight,
-    worldWidth: props.map.pointWidth(),
-    worldHeight: props.map.pointHeight(),
-    passiveWheel: false,
-    disableOnContextMenu: true
-  });
+    result.stage.addChild(viewport);
 
-  app.stage.addChild(viewport);
-  viewport.
-    drag().
-    wheel().
-    pinch().
-    clampZoom({minScale: 0.1, maxScale: 1}).
-    clamp({direction: "all"}).
-    zoomPercent(-0.9).
-    moveCenter(props.map.pointWidth() * 0.5, props.map.pointHeight() * 0.5);
+    viewport.
+      drag().
+      wheel().
+      pinch().
+      clampZoom({minScale: 0.1, maxScale: 1}).
+      clamp({direction: "all"}).
+      zoomPercent(-0.4).
+      moveCenter(props.map.pointWidth() * 0.5, props.map.pointHeight() * 0.5);
 
-  let message = new PIXI.Text("Tiny King", new PIXI.TextStyle({ fill: "white" }));
-  message.position.set(window.innerWidth * 0.5, 50);
-  message.anchor.x = 0.5;
-  app.stage.addChild(message);
+    const terrainColours = {
+      "mountain": 0x3C3A44,
+      "water": 0x3F6FAE,
+      "grassland": 0x80C05D,
+      "forest": 0x30512F,
+      "stone": 0x5D7084,
+    };
 
-  let disclaimer = new PIXI.Text("Technical Demo.\nAll features in very early stages and subject to change.\nCopyright (c) 2020 Think Code Learn Ltd t/a Revelation Games", new PIXI.TextStyle({ fill: "white", fontSize: 9 }));
-  disclaimer.position.set(3, window.innerHeight - 3);
-  disclaimer.anchor.y = 1.0;
-  app.stage.addChild(disclaimer);
+    window.onresize = () => {
+      result.renderer.resize(window.innerWidth, window.innerHeight);
+    };
 
-  const terrainColours = {
-    "mountain": 0x3C3A44,
-    "water": 0x3F6FAE,
-    "grassland": 0x80C05D,
-    "forest": 0x30512F,
-    "stone": 0x5D7084,
+    // Render
+    props.map.forEach(hex => {
+      const graphics = new PIXI.Graphics();
+
+      const point = hex.toPoint();
+      const corners = hex.corners().map(corner => corner.add(point));
+
+      graphics.beginFill(terrainColours[hex.tile.terrain]);
+      graphics.lineStyle({color: "black", width: 2, alpha: 0.04});
+      graphics.drawPolygon(...corners);
+      graphics.endFill();
+
+      viewport.addChild(graphics);
+    });
+    return result;
   };
 
-  window.onresize = () => {
-    app.renderer.resize(window.innerWidth, window.innerHeight);
-    message.position.set(window.innerWidth * 0.5, 50);
-    disclaimer.position.set(3, window.innerHeight - 3);
-  };
-
-  // Render
-  props.map.forEach(hex => {
-    const graphics = new PIXI.Graphics();
-
-    const point = hex.toPoint();
-    const corners = hex.corners().map(corner => corner.add(point));
-
-    graphics.beginFill(terrainColours[hex.tile.terrain]);
-    graphics.lineStyle({color: "black", width: 2, alpha: 0.04});
-    graphics.drawPolygon(...corners);
-    graphics.endFill();
-
-    viewport.addChild(graphics);
+  React.useLayoutEffect(() => {
+    setApp(app || createApp(container.current));
+    return function cleanup() {
+      if (app) {
+        app.destroy({children: true, texture: true, baseTexture: true});
+      }
+    };
   });
 
-  React.useEffect(() => {
-    container.current.appendChild(app.view);
-  });
-
-  return (<div ref={container}></div>);
+  return (<canvas ref={container}></canvas>);
 }
+
+World.propTypes = {
+  map: PropTypes.array, // Actually a Honeycomb grid but close enough
+};
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./style.scss";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Honeycomb from "honeycomb-grid";
+import * as Honeycomb from "honeycomb-grid";
 
 import { World } from "./components/world";
 import * as mapgen from "mapgen";
@@ -26,6 +26,8 @@ const map = Grid.rectangle({width: mapRadius * 2, height: mapRadius * 2});
 mapgen.generate(map, seed);
 
 ReactDOM.render(
-  <div>
+  <div id='game'>
     <World map={map}/>
+    <h1 className='header'>Tiny King</h1>
+    <div className='disclaimer'>Technical Demo.<br/>All features in very early stages and subject to change.<br/>Copyright (c) 2020 Think Code Learn Ltd t/a Revelation Games</div>
   </div>, document.getElementById("root"));

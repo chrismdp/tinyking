33303012efc823a032c9790755a24a7f296390a0
Chris Parsons <cp@gower.st>
2020-07-20 07:17:29 +0100

    A few styling tweaks

--- src/components/game.js
+++ src/components/game.js
@@ -13,7 +13,7 @@ export function Game({map, seed, changeSeed, width, height}) {
       <div className='disclaimer'>Technical Demo {VERSION}<br/>All features in very early stages and subject to change.<br/>Copyright (c) 2020 Think Code Learn Ltd t/a Revelation Games</div>
       <div className='social'>
         <a href='https://github.com/chrismdp/tinyking/' target='_blank' rel='noreferrer'><img src='https://img.shields.io/github/stars/chrismdp/tinyking?color=417154&label=github&logo=github1'/></a>
-        <a href='https://discord.gg/ZgXcVyn' target='_blank' rel='noreferrer'><img src='https://img.shields.io/discord/731912590489288795?color=blue&label=discord'/></a>
+        <a href='https://discord.gg/ZgXcVyn' target='_blank' rel='noreferrer'><img src='https://img.shields.io/discord/731912590489288795?color=417154&label=discord'/></a>
       </div>
     </div>);
 }

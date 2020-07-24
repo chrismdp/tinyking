4cc8e3c8dfa2e90aad90354687aff234c48e1ca7
Chris Parsons <cp@gower.st>
2020-07-24 15:05:52 +0100

    Draggable panels

--- src/components/mapgen.js
+++ src/components/mapgen.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import Draggable from "react-draggable";
+
 import { toggleDebugMapLayer } from "features/ui_slice";
 import { getMapSeed, generate } from "features/map_slice";
 
@@ -19,21 +21,23 @@ export function MapGenParams() {
     dispatch(generate({ seed })), [dispatch]);
 
   return (
-    <div className='panel'>
-      <h1>Map generation</h1>
-      <div className='row'>
-        <label htmlFor='seed'>Random seed:</label>
-        <input id='seed' type='text' ref={input} defaultValue={seed}/>
-        <button onClick={() => input.current.value = Math.round(Math.random() * 10000000)}><FontAwesomeIcon icon="dice"/></button>
+    <Draggable handle=".handle" bounds="parent">
+      <div className='panel'>
+        <h1 className="handle">Map generation</h1>
+        <div className='row'>
+          <label htmlFor='seed'>Random seed:</label>
+          <input id='seed' type='text' ref={input} defaultValue={seed}/>
+          <button onClick={() => input.current.value = Math.round(Math.random() * 10000000)}><FontAwesomeIcon icon="dice"/></button>
+        </div>
+        <div className='row'>
+          <label htmlFor='mapLayer'>Show debug map layer:</label>
+          <input id='mapLayer' type='checkbox' defaultValue={mapLayer} onChange={clickedMapLayer}/>
+        </div>
+        <div className='row'>
+          <button onClick={() => clickedGenerate(input.current.value)}>Generate map</button>
+          { progress.label && <div className='progress'>{Math.round(progress.progress * 100)}% {progress.label}</div> }
+        </div>
       </div>
-      <div className='row'>
-        <label htmlFor='mapLayer'>Show debug map layer:</label>
-        <input id='mapLayer' type='checkbox' defaultValue={mapLayer} onChange={clickedMapLayer}/>
-      </div>
-      <div className='row'>
-        <button onClick={() => clickedGenerate(input.current.value)}>Generate map</button>
-        { progress.label && <div className='progress'>{Math.round(progress.progress * 100)}% {progress.label}</div> }
-      </div>
-    </div>
+    </Draggable>
   );
 }

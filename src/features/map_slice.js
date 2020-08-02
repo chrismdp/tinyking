4b7e52897e16dc689f8e47622aee5a3c6fa76cd6
Chris Parsons <cp@gower.st>
2020-08-02 16:07:25 +0100

    Move family code back into map
    It doesn't really belong in its own slice. I'm thinking each of the component types probably deserve their own slice at some point too: but not yet.

--- src/features/map_slice.js
+++ src/features/map_slice.js
@@ -1,7 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { select, delay, put, putResolve, call } from "redux-saga/effects";
-import { clearEntities, newEntities, getPlayerId, discoverTiles } from "features/entities_slice";
-import { generateFamilies } from "features/families_slice";
+import { getAllComponentsWithXY, clearEntities, newEntities, getPlayerId, discoverTiles } from "features/entities_slice";
 
 import * as Honeycomb from "honeycomb-grid";
 import * as SimplexNoise from "simplex-noise";
@@ -43,6 +42,54 @@ const mapSlice = createSlice({
     }
   }
 });
+
+const HAIR = {
+  red: 0x8D3633,
+  brown: 0x292021,
+  black: 0x20232A,
+  blonde: 0x957E3C
+};
+const hair = Object.values(HAIR);
+
+const BODY_MALE = 0x4E3F30;
+const BODY_FEMALE = 0x3B6071;
+
+function generateFamily(size, x, y, generator) {
+  var result = [];
+  for (var p = 0; p < size; p++) {
+    result.push({
+      nameable: { type: "person", seed: generator.random_int() },
+      renderable: {
+        x, y,
+        type: "person",
+        familyIndex: p / (size * 1.5),
+        size: p > 1 ? 12 : 20,
+        hair: hair[generator.random_int() % hair.length],
+        body: generator.random_int() % 2 == 0 ? BODY_MALE : BODY_FEMALE
+      }
+    });
+  }
+  return result;
+}
+
+export function* generateFamilies({ seed, playerStart }) {
+  var people = [];
+  var generator = new MersenneTwister(seed);
+  var habitables = yield select(getAllComponentsWithXY("habitable"));
+  for (var i = 0; i < habitables.length; i++) {
+    var habitable = habitables[i];
+    if (habitable.x == playerStart.x && habitable.y == playerStart.y) {
+      const player = { ...generateFamily(1, habitable.x, habitable.y, generator)[0],
+        playable: { known: [] }
+      };
+      people = [ ...people, player ];
+    } else {
+      const familySize = 1 + (generator.random_int() % 5);
+      people = [ ...people, ...generateFamily(familySize, habitable.x, habitable.y, generator) ];
+    }
+  }
+  yield put(newEntities(people));
+}
 
 export const Hex = Honeycomb.extendHex({
   size: HEX_SIZE,

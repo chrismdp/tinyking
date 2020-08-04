53fc2d34be872cb12f37017da9a4d64e07da8341
Chris Parsons <cp@gower.st>
2020-08-04 08:32:13 +0100

    More front page tweaks

--- src/components/game.js
+++ src/components/game.js
@@ -15,19 +15,17 @@ export function Game() {
         <div className="header">
           <div className="small">Welcome to</div>
           <div className="main">Tiny King</div>
-          <div className="strapline">The kingdom builder that&rsquo;s all about the people.<br/>Placeholder art, Alpha gameplay, open development.</div>
+          <div className="strapline">The kingdom builder that&rsquo;s all about the people.<br/>Placeholder art, alpha gameplay, open development.</div>
         </div>
         <menu>
-          <a>Continue this game</a><br/>
-          <a>Create a custom map</a><br/>
+          <li><a>Quick start</a></li>
+          <li><a>Create a custom map</a></li>
           <div className="social">
             <a href="https://discord.gg/ZgXcVyn" target="_blank" rel="noreferrer"><img src="https://img.shields.io/discord/731912590489288795?color=417154&label=discord"/></a>
             <a href="https://github.com/chrismdp/tinyking/" target="_blank" rel="noreferrer"><img src="https://img.shields.io/github/stars/chrismdp/tinyking?color=417154&label=github&logo=github1"/></a>
           </div>
           <div className="disclaimer">Technical Demo {VERSION}<br/>Copyright &copy; 2020 Think Code Learn Ltd t/a Revelation Games</div>
         </menu>
-      </div>
-      <div className="social">
       </div>
     </div>
   );

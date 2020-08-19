19d0ffab2b8325cc88ef50e9f9af67d01df37116
Chris Parsons <cp@gower.st>
2020-08-19 09:56:11 +0100

    Popup style improvements

--- src/components/info.js
+++ src/components/info.js
@@ -7,13 +7,15 @@ import { fullEntity } from "game/entities";
 export function Info({ entityId }) {
   const state = React.useContext(GameState);
   const entity = React.useMemo(() => fullEntity(state.ecs, entityId), [state.ecs, entityId]);
-  const title = entity.nameable ? (<Name nameable={entity.nameable}/>) : "Information";
+  const title = entity.mappable ? (entity.mappable.terrain) : (entity.nameable ? (<Name nameable={entity.nameable}/>) : "Information");
+  const iControl = state.ecs.playable[state.ui.playerId].controls.includes(entityId);
 
   return (
     <div>
-      <h1 className="handle">{title}</h1>
-      { entity.mappable && (<div>Terrain type: <strong>{entity.mappable.terrain}</strong></div>) }
+      {title && (<h1 className="capitalise handle">{title}</h1>)}
       { entity.traits && entity.traits.values.length > 0 && (<div>Traits: <strong>{ entity.traits.values.join(", ") }</strong></div>) }
+      { entity.habitable && entity.habitable.owners.map({o => state.ecs.nameable[o].name}
+      { iControl && (<p>You control this character. Click and drag to assign to a job.</p>) }
     </div>
   );
 }
--- src/game/entities.js
+++ src/game/entities.js
@@ -1,6 +1,6 @@
 export function newEntities(ecs, entities) {
   return entities.map(entity => {
-    const id = ecs.nextId++;
+    const id = "" + (ecs.nextId++);
     for (const name in entity) {
       if (!ecs[name]) {
         ecs[name] = {};

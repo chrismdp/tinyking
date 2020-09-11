7e6991ee8376f7a5c853af67d4aec44406fdf69e
Chris Parsons <cp@gower.st>
2020-09-11 14:59:39 +0100

    Improve supplies display

--- src/components/supplies.js
+++ src/components/supplies.js
@@ -6,5 +6,9 @@ export function Supplies() {
   const state = React.useContext(GameState);
   const supplies = state.ecs.supplies[state.ui.playerId];
 
-  return (<div id="supplies">{JSON.stringify(supplies)}</div>);
+  return (<div id="supplies" className="capitalise">{
+    Object.keys(supplies)
+      .filter(k => k != "id")
+      .map(k => k + ": " + supplies[k]).join(", ")
+  }</div>);
 }

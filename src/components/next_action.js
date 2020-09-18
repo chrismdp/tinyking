cc18e1f35c57ece5f672313cc9494d39d8ef6d03
Chris Parsons <cp@gower.st>
2020-09-18 13:00:33 +0100

    Styling space for selected person

--- src/components/next_action.js
+++ src/components/next_action.js
@@ -9,14 +9,17 @@ export function NextAction() {
   const haveMovesOrActions = [
     state.ui.playerId,
     ...directlyControlledBy(state.ecs, state.ui.playerId).map(p => p.id)
-  ].filter(id => state.ecs.attributes[id].moves + state.ecs.attributes[id].actions > 0)
+  ].filter(id => state.ecs.attributes[id].moves > 0 || !state.ecs.assignable.task)
     .length;
 
   const t = useTranslate();
 
   return (
     <div id="next-action">
-      <button onClick={state.ui.actions.end_turn}>{t(haveMovesOrActions > 0 ? "next_action.left" : "next_action.end_turn", { to_assign: haveMovesOrActions})}</button>
+      <button onClick={state.ui.actions.end_turn}>
+        {t(haveMovesOrActions > 0 ? "next_action.left" : "next_action.end_turn",
+          { to_assign: haveMovesOrActions})}
+      </button>
     </div>
   );
 }

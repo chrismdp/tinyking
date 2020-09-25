3739cf5beb212a30c6dab3f728538fa7d75bbaab
Chris Parsons <cp@gower.st>
2020-09-25 15:43:09 +0100

    More robust links to allow proper pathfinding

--- src/game/entities.js
+++ src/game/entities.js
@@ -1,13 +1,25 @@
-export function newEntities(ecs, entities) {
+import { Hex } from "game/map";
+
+export function newEntities(state, entities) {
   return entities.map(entity => {
-    const id = "" + (ecs.nextId++);
+    const id = "" + (state.ecs.nextId++);
     for (const name in entity) {
-      if (!ecs[name]) {
-        ecs[name] = {};
+      if (!state.ecs[name]) {
+        state.ecs[name] = {};
       }
       if (entity[name]) {
-        ecs[name][id] = { ...entity[name], id };
+        state.ecs[name][id] = { ...entity[name], id };
       }
+    }
+    if (entity.spatial) {
+      const hex = Hex().fromPoint(entity.spatial);
+      if (!state.space) {
+        state.space = {};
+      }
+      if (!(hex in state.space)) {
+        state.space[hex] = [];
+      }
+      state.space[hex].push(id);
     }
     return id;
   });

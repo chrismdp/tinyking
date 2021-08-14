e14820752569e75a867c4a76a323364e64a93f91
Chris Parsons <chris@thinkcodelearn.com>
2021-08-15 00:36:26 +0100

    Update year to current

--- src/components/main_menu.js
+++ src/components/main_menu.js
@@ -5,6 +5,8 @@ import { GameState } from "components/contexts";
 
 export function MainMenu({ show }) {
   const state = React.useContext(GameState);
+  const year = new Date().getFullYear();
+
   return (
     <div id="main-menu" className={show ? "" : "hiding"}>
       <div className="header">
@@ -18,7 +20,10 @@ export function MainMenu({ show }) {
           <a href="https://discord.gg/ZgXcVyn" target="_blank" rel="noreferrer"><img src="https://img.shields.io/discord/731912590489288795?color=417154&label=discord"/></a>
           <a href="https://github.com/chrismdp/tinyking/" target="_blank" rel="noreferrer"><img src="https://img.shields.io/github/stars/chrismdp/tinyking?color=417154&label=github&logo=github1"/></a>
         </div>
-        <div className="disclaimer">Technical Demo {VERSION}<br/>Copyright &copy; 2020 Think Code Learn Ltd t/a Revelation Games</div>
+        <div className="disclaimer">
+          Technical Demo {VERSION}
+          <br/>
+          Copyright &copy; {year} Think Code Learn Ltd t/a Revelation Games</div>
       </menu>
     </div>);
 }

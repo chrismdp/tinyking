5230238a86a51761e76fc8bb0714d5bc83d764a9
Chris Parsons <chris@thinkcodelearn.com>
2022-06-15 00:42:23 +0100

    Honeycomb

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import * as Honeycomb from 'honeycomb-grid';
+
+const Hex = Honeycomb.extendHex({
+  size: 1,
+  orientation: 'pointy'
+});
 
 function App() {
   return (

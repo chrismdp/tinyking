094799027e82ddee1babdb4a76f79ecaf6003ded
Chris Parsons <chris@thinkcodelearn.com>
2022-06-19 14:23:35 +0100

    Refactor buildings too

--- src/features/map/Building.js
+++ src/features/map/Building.js
@@ -1,15 +1,12 @@
 import { Hex } from "./hex.js";
 
-import Unit_house from "../../models/Unit_house.js"
+import * as Buildings from "../../models/Buildings.js"
 
-// TODO: Really necessary to have this layer of indirection here?
-const COMPONENTS = {
-  "house": Unit_house,
-}
+import buildings from "../../data/buildings.json"
 
 export default function Building({x, y, type, ...props}) {
   const point = Hex(x, y).toPoint();
-  const Component = COMPONENTS[type];
+  const Component = Buildings[buildings[type].component];
   if (Component) {
     return (
       <Component position={[point.x, 0.2,  point.y]} {...props}/>

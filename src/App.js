785ef20dac167d87853b3092e0e49cfb87917a96
Chris Parsons <chris@thinkcodelearn.com>
2022-07-04 21:31:39 +0100

    Basic translation of event conditions

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import EventTable from "./EventTable.js";
 import Game from "./Game.js";
 
+import TRANSLATIONS from "./data/strings.en.json";
+
 import './App.css';
+import { I18n } from "react-polyglot";
+
+const locale = window.locale || "en";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/tools/events" element={<EventTable/>}/>
-        <Route exact path="/" element={<Game/>}/>
-        <Route path="*" element={<div className="p-5">Path not found</div>}/>
-      </Routes>
-    </BrowserRouter>
+    <I18n locale={locale} messages={TRANSLATIONS}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/tools/events" element={<EventTable/>}/>
+          <Route exact path="/" element={<Game/>}/>
+          <Route path="*" element={<div className="p-5">Path not found</div>}/>
+        </Routes>
+      </BrowserRouter>
+    </I18n>
   );
 }
 
--- src/features/ui/EventPanel.js
+++ src/features/ui/EventPanel.js
@@ -1,4 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useTranslate } from 'react-polyglot';
 
 import { animated, useSpring, config } from '@react-spring/web';
 import { addTile } from "../map/mapSlice.js";
@@ -15,7 +16,7 @@ export default function EventPanel({ visible }) {
   const dispatch = useDispatch();
 
   const { event, hex, terrain } = useSelector(state => state.ui.panel);
-  const { heading, text, prompts } = EVENTS[event] || {};
+  const { heading, text, prompts, conditions } = EVENTS[event] || {};
 
   const image = terrain && TERRAINS[terrain].image;
 
@@ -24,16 +25,30 @@ export default function EventPanel({ visible }) {
     config: config.stiff
   });
 
+  const t = useTranslate();
+
   let classes = "z-top absolute bg-neutral-800 p-4 inset-x-0 bottom-0 md:bottom-10 md:inset-x-10";
 
   return (
     <animated.div style={styles} className={classes}>
       <div className="relative">
-        { image && <img className="absolute pointer-events-none object-bottom shadow-lg object-cover -right-24 bottom-16 w-80" src={`/images/${image}.png`} alt={terrain}/> }
+        { image && <img className="absolute pointer-events-none object-bottom object-cover -right-24 hidden sm:block -top-56 w-80" src={`/images/${image}.png`} alt={terrain}/> }
         <h1 className="font-title text-2xl">{heading}</h1>
         <p className="pt-2 mr-20">{text}</p>
+        { conditions &&
+          <ul className="flex py-2 text-xs">
+            { Object.keys(conditions).flatMap(k1 =>
+              Object.keys(conditions[k1]).map(k2 => {
+                const key = [ "conditions", k1, k2 ].join(".");
+                return <li key={key} className='mx-1 bg-gray-700 py-1 px-2 rounded'>
+                  {t(key, { value: conditions[k1][k2] }) }
+                </li>
+              })
+            )}
+          </ul>
+        }
         { prompts &&
-          <div className="py-4">
+          <div className="py-2">
             { prompts.map((prompt, idx) =>
               <button className="px-4 py-2 hover:bg-blue-800 bg-blue-900 mr-2 rounded-lg" key={idx} onClick={() => dispatch(actionFor(terrain, hex, prompt))}>{prompt.label}</button>)
             }
